b6a6a1a28d955783243cbabcb448715511af33a6
Ryan Ogden <ryan@expandtheroom.com>
2016-12-12 17:05:52 -0500

    Fix typo in constant

--- Theme_Support/favicon.php
+++ Theme_Support/favicon.php
@@ -1,7 +1,7 @@
 <?php
 
 add_filter( 'wp_head', function() {
-    $favicon_url = get_theme_support( BP\Theme_Support\BP\Theme_Support\Theme_Support::FAVCION );
+    $favicon_url = get_theme_support( BP\Theme_Support\BP\Theme_Support\Theme_Support::FAVICON );
 
     echo '<link rel="icon" type="image/png" href="' . $favicon_url . '">';
 });
