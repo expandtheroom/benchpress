cfa2ff88c4c277e0b3f5bc653b8a376111925180
Ryan Ogden <ryan@expandtheroom.com>
2016-12-12 17:54:36 -0500

    Formatting

--- Theme_Support/google-analytics.php
+++ Theme_Support/google-analytics.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('wp_footer', function() {
+add_action( 'wp_footer', function() {
     $args = get_theme_support( BenchPress\Theme_Support\Theme_Support::GOOGLE_ANALYTICS );
     $ua = $args[0];
     ?>
